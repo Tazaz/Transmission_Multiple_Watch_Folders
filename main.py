1a300687ad34c03d4952d543b1290d51fb663749
Tony Zaremba <taz@sunz.eu>
2013-07-09 20:42:17 +0200

    2nd update

--- main.py
+++ main.py
@@ -13,34 +13,26 @@ watch_tv = ''
 watch_movie = ''
 download_dir_tv = ''
 download_dir_movie = ''
+backup_dir = 'remove'
 
-<<<<<<< HEAD
-=======
 
->>>>>>> small update
-def add(watch_dir, download_dir):
+def add(watch_dir, download_dir, after):
     watch_dir = os.listdir(watch_tv)
     if watch_dir != []:
         for i in watch_dir:
-<<<<<<< HEAD
-            print watch_tv + '/' + i
-            client.add_torrent(watch_tv + '/' + i, download_dir=download_dir)
-            time.sleep(1)
-            os.remove(watch_tv + '/' + i)
-            time.sleep(1)
-=======
             if i.lower().endswith('.torrent'):
-                client.add_torrent(watch_tv + '/' + i, download_dir=download_dir)
+                try:
+                    client.add_torrent(watch_tv + '/' + i, download_dir=download_dir)
+                    time.sleep(1)
+                    if after == 'remove':
+                        os.remove(watch_tv + '/' + i)
+                    else:
+                        os.rename(watch_tv + '/' + i, after + '/' + i)
+                except:
+                    print 'Unexpected error'
                 time.sleep(1)
-                os.remove(watch_tv + '/' + i)
-                time.sleep(1)
->>>>>>> small update
 
 while True:
-    add(watch_tv, download_dir_tv)
-    add(watch_movie, download_dir_movie)
-<<<<<<< HEAD
+    add(watch_tv, download_dir_tv, backup_dir)
+    add(watch_movie, download_dir_movie, backup_dir)
     time.sleep(60)
-=======
-    time.sleep(60)
->>>>>>> small update
